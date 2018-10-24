9ca508ad72c8decdee0d1324ddec435513ddf653
mac <macarthy@hotmail.fr>
2018-10-24 14:31:11 +0200

    le1

---
+++ 1d-mol.py
@@ -0,0 +1,20 @@
+#/usr/local/bin/python3
+
+import random
+
+chiffreale.random(0, 100)
+
+print("entre un chiffre")
+
+chiffrechoisit = input()
+chiffrechoisit
+while chiffrechoisit != chiffreale:
+  if chiffrechoisit > chiffreale:
+    print("plus petit")
+    chiffrechoisit
+  elif chiffrechoisit < chiffreale:
+    print("plus grand")
+    chiffrechoisit
+    break
+ print ("bravo")
+
